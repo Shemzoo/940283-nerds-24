97b264ac65be80c0e1798e8d0fa4ea44c8e192c4
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2019-03-22 16:07:26 +0300

    Last Fixes 0.3

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+var link = document.querySelector(".write-us-button");
+
+var popup = document.querySelector(".modal-write-us");
+
+var close = popup.querySelector(".btn-close");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
